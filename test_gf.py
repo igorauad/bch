7996c4a4aa5ad77ca05249fd4e2482b8b7d9391f
Igor Freire <igor.auad@gmail.com>
2022-04-16 16:23:41 -0300

    Create GF class with LUTs and helpers
    Create a class to hold both the direct GF table (from index to elements
    values) and the inverse LUT (from values to indexes). Add also helper
    methods on the class to facilitate conversion between values, exponents,
    and indexes. Use the instance of this new class where possible as a
    replacement for passing both LUTs independently.

--- test_gf.py
+++ test_gf.py
@@ -59,7 +59,7 @@ class TestGf(unittest.TestCase):
         """Confirm the bit-packed implementation produces identical results"""
         for m, poly_str in zip([4, 6], ["11001", "1100001"]):
             gf_table_unpacked = gf.gf(m, poly_str)
-            gf_table_packed = gf.gf_packed(m, poly_str)
+            field = gf.GF(m, poly_str)
             # Convert the non-packed table to its packed equivalent. First, pad
             # each row to length 8 by adding (8 - m) columns to the left.
             # Otherwise, np.packbits would pad columns to the right instead.
@@ -68,23 +68,22 @@ class TestGf(unittest.TestCase):
             # Make sure the two implementations match. The packed
             # implementation returns a uint32 1-D array.
             np.testing.assert_array_equal(
-                expected_table.astype(np.uint32).reshape(-1), gf_table_packed)
+                expected_table.astype(np.uint32).reshape(-1), field.table)
 
     def test_elem_to_index_lut(self):
         m = 4
         poly_str = "11001"
-        gf_table = gf.gf_packed(m, poly_str)
-        inv_gf_table = gf.elem_to_index_lut(gf_table)
+        field = gf.GF(m, poly_str)
 
         # Look-up and inverse look-ups work
-        for elem in gf_table:
-            exponent = inv_gf_table[elem]
-            self.assertEqual(gf_table[exponent], elem)
+        for elem in field.table:
+            exponent = field.inv_table[elem]
+            self.assertEqual(field.table[exponent], elem)
 
         # The first element is an exception. By convention, it will be zero in
         # the inverse LUT. By definition, it is zero in the original table.
-        self.assertEqual(gf_table[0], 0)
-        self.assertEqual(inv_gf_table[0], 0)
+        self.assertEqual(field.table[0], 0)
+        self.assertEqual(field.inv_table[0], 0)
 
         # Since the convention is to represent the coefficient of lowest power
         # on the left of the int number representing alpha^i, it follows that
@@ -99,15 +98,14 @@ class TestGf(unittest.TestCase):
         # associated with exponent 1 is at index=2 (the third position), and so
         # on. In other words, the index on the original GF table is equal to
         # the exponent plus one. Hence, index=m represents exponent "m - 1".
-        self.assertEqual(gf_table[m], 1)
-        self.assertEqual(inv_gf_table[1], m)
+        self.assertEqual(field.table[m], 1)
+        self.assertEqual(field.inv_table[1], m)
 
     def test_multiply(self):
         # See some cases in Example 2.7
         m = 4
         poly_str = "11001"
-        gf_table = gf.gf_packed(m, poly_str)
-        inv_gf_table = gf.elem_to_index_lut(gf_table)
+        field = gf.GF(m, poly_str)
         multiplicand_exponents = [
             (0, 4),
             (1, 4),
@@ -126,62 +124,57 @@ class TestGf(unittest.TestCase):
         ]
         for (exp_a, exp_b), exp_z in zip(multiplicand_exponents,
                                          result_exponents):
-            # Get the elements for each exponent while noting alpha^0 is at
-            # gf_table[1], alpha^1 at gf_table[2], and so on.
-            a = gf_table[exp_a + 1]
-            b = gf_table[exp_b + 1]
-            z = gf_table[exp_z + 1]
-            self.assertAlmostEqual(gf.multiply(gf_table, inv_gf_table, a, b),
-                                   z)
+            # Get the elements for each exponent
+            a = field.get_element(exp_a)
+            b = field.get_element(exp_b)
+            z = field.get_element(exp_z)
+            self.assertAlmostEqual(gf.multiply(field, a, b), z)
         # Multiplication by zero should always result in 0
         for x in range(2**m):
-            self.assertAlmostEqual(gf.multiply(gf_table, inv_gf_table, x, 0),
-                                   0)
+            self.assertAlmostEqual(gf.multiply(field, x, 0), 0)
 
     def test_multiplicative_inverse(self):
         m = 4
         poly_str = "11001"
-        gf_table = gf.gf_packed(m, poly_str)
-        inv_gf_table = gf.elem_to_index_lut(gf_table)
+        field = gf.GF(m, poly_str)
 
         # See some cases in Example 2.7
-        elem = gf_table[12 + 1]  # alpha^12
-        inv = gf_table[3 + 1]  # alpha^3
-        self.assertEqual(gf.inverse(gf_table, inv_gf_table, elem), inv)
+        elem = field.get_element(12)  # alpha^12
+        inv = field.get_element(3)  # alpha^3
+        self.assertEqual(gf.inverse(field, elem), inv)
 
-        elem = gf_table[5 + 1]  # alpha^5
-        inv = gf_table[10 + 1]  # alpha^10
-        self.assertEqual(gf.inverse(gf_table, inv_gf_table, elem), inv)
+        elem = field.get_element(5)  # alpha^5
+        inv = field.get_element(10)  # alpha^10
+        self.assertEqual(gf.inverse(field, elem), inv)
 
-        elem = gf_table[1]  # alpha^0
-        inv = gf_table[1]  # alpha^0
-        self.assertEqual(gf.inverse(gf_table, inv_gf_table, elem), inv)
+        elem = field.get_element(0)  # alpha^0
+        inv = field.get_element(0)  # alpha^0
+        self.assertEqual(gf.inverse(field, elem), inv)
 
-        elem = gf_table[15]  # alpha^14
-        inv = gf_table[2]  # alpha^1
-        self.assertEqual(gf.inverse(gf_table, inv_gf_table, elem), inv)
+        elem = field.get_element(14)  # alpha^14
+        inv = field.get_element(1)  # alpha^1
+        self.assertEqual(gf.inverse(field, elem), inv)
 
     def test_division(self):
         m = 4
         poly_str = "11001"
-        gf_table = gf.gf_packed(m, poly_str)
-        inv_gf_table = gf.elem_to_index_lut(gf_table)
+        field = gf.GF(m, poly_str)
 
         # See some cases in Example 2.7
-        a = gf_table[4 + 1]  # alpha^4
-        b = gf_table[12 + 1]  # alpha^12
-        res = gf_table[7 + 1]  # alpha^7
-        self.assertEqual(gf.divide(gf_table, inv_gf_table, a, b), res)
+        a = field.get_element(4)  # alpha^4
+        b = field.get_element(12)  # alpha^12
+        res = field.get_element(7)  # alpha^7
+        self.assertEqual(gf.divide(field, a, b), res)
 
-        a = gf_table[12 + 1]  # alpha^12
-        b = gf_table[5 + 1]  # alpha^5
-        res = gf_table[7 + 1]  # alpha^7
-        self.assertEqual(gf.divide(gf_table, inv_gf_table, a, b), res)
+        a = field.get_element(12)  # alpha^12
+        b = field.get_element(5)  # alpha^5
+        res = field.get_element(7)  # alpha^7
+        self.assertEqual(gf.divide(field, a, b), res)
 
-        a = gf_table[12 + 1]  # alpha^12
-        b = gf_table[1]  # alpha^0
-        res = gf_table[12 + 1]  # alpha^12
-        self.assertEqual(gf.divide(gf_table, inv_gf_table, a, b), res)
+        a = field.get_element(12)  # alpha^12
+        b = field.get_element(0)  # alpha^0
+        res = field.get_element(12)  # alpha^12
+        self.assertEqual(gf.divide(field, a, b), res)
 
     def test_conjugates(self):
         # See Table 2.9
@@ -200,28 +193,21 @@ class TestGf(unittest.TestCase):
         # See Table 2.9
         m = 4
         poly_str = "11001"
-        gf_table = gf.gf_packed(m, poly_str)
-        inv_gf_table = gf.elem_to_index_lut(gf_table)
+        field = gf.GF(m, poly_str)
 
         # Trivial minimal polynomials
-        self.assertEqual(
-            gf.min_polynomial(gf_table, inv_gf_table, gf_table[0]), [1, 0])
-        self.assertEqual(
-            gf.min_polynomial(gf_table, inv_gf_table, gf_table[1]), [1, 1])
+        self.assertEqual(gf.min_polynomial(field, field.table[0]), [1, 0])
+        self.assertEqual(gf.min_polynomial(field, field.table[1]), [1, 1])
 
         for i in [1, 2, 4, 8]:
-            beta = gf_table[i + 1]  # alpha^i
-            self.assertEqual(gf.min_polynomial(gf_table, inv_gf_table, beta),
-                             [1, 0, 0, 1, 1])
+            beta = field.get_element(i)  # alpha^i
+            self.assertEqual(gf.min_polynomial(field, beta), [1, 0, 0, 1, 1])
         for i in [3, 6, 9, 12]:
-            beta = gf_table[i + 1]  # alpha^i
-            self.assertEqual(gf.min_polynomial(gf_table, inv_gf_table, beta),
-                             [1, 1, 1, 1, 1])
+            beta = field.get_element(i)  # alpha^i
+            self.assertEqual(gf.min_polynomial(field, beta), [1, 1, 1, 1, 1])
         for i in [5, 10]:
-            beta = gf_table[i + 1]  # alpha^i
-            self.assertEqual(gf.min_polynomial(gf_table, inv_gf_table, beta),
-                             [1, 1, 1])
+            beta = field.get_element(i)  # alpha^i
+            self.assertEqual(gf.min_polynomial(field, beta), [1, 1, 1])
         for i in [7, 11, 13, 14]:
-            beta = gf_table[i + 1]  # alpha^i
-            self.assertEqual(gf.min_polynomial(gf_table, inv_gf_table, beta),
-                             [1, 1, 0, 0, 1])
+            beta = field.get_element(i)  # alpha^i
+            self.assertEqual(gf.min_polynomial(field, beta), [1, 1, 0, 0, 1])
